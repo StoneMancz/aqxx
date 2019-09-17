8d623c232d54f8b8331261654bbe2a93214c18c5
StoneMancz <15173406470@163.com>
2019-09-17 10:29:19 +0800

    初始化项目

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>这是欢迎页面</Text>
+    </View>
+  );
+};
+export default App;
